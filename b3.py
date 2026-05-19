9d93f3b1c15ad541dd28ceb4c44be1deee5c0ded
vulananh29112007-art <vulananh29112007@gmail.com>
2026-05-19 12:30:54 +0700

    Update b3.py
    Added comments to clarify input and output handling.

--- b3.py
+++ b3.py
@@ -16,6 +16,11 @@ elif age >= 80:
 else:
     send_messenger = "KHÁM THƯỜNG: Vui lòng lấy số thứ tự và chờ tới lượt tại sảnh."
 
+# dữ liệu đầu vào gồm: tên bệnh nhân, tuổi, 
+# đầu ra là chia tuổi theo điều kkiệnddeer xác định độ ưu tiên 
+# mô tả phân luồng: kiểm tra điều kiện lỗi trước, nếu thuộc 1 trong các điều kiện sai -> dừng chương trình luôn
+# nếu thuộc điều kiện đúng thì thuộc hiện chia mức độ ưu tiên
+
 print(f"""
 ===== PHIẾU KHÁM BỆNH ĐIỆN TỬ =====
 
